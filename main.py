574360bc913b83edfe66d294f4de3dc620a70de1
Matteo <mattecalautti@gmail.com>
2026-02-15 09:31:31 +0100

    refactor for homeservice manager

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 Initial module where basic components like inputs and outputs are initialized
 and callback functions are loaded into the response pipeline
 """
-import time
-
 from system import Configurations as Conf, ConfigKey
 from system import OutputPipelineManager as Pipeline
 from log_manager import setup_logger
@@ -38,40 +36,36 @@ def print_service_status():
         log_message = "MQTT mode OFF"
     logger.info(log_message)
 
+
+network.NetworkManager.mqtt_connect_to_broker(
+    username=Conf.get_conf_data(ConfigKey.MQTT_USERNAME),
+    password=Conf.get_conf_data(ConfigKey.MQTT_PASSWORD),
+    host=Conf.get_conf_data(ConfigKey.MQTT_HOST),
+    port=Conf.get_conf_data(ConfigKey.MQTT_PORT)
+)
+
+#MIC_NAME and RECOGNITION_LANGUAGE will be a list to init more then one mic at time
+audio_input.mic_list.append(
+    audio_input.MicManager(mic_name=Conf.get_conf_data(ConfigKey.MIC_NAME),
+        recognition_language=Conf.get_conf_data(ConfigKey.RECOGNITION_LANGUAGE))
+)
+
+#OUT_DEVICE_NAME will be a list to init more then one mic at time
+audio_output.audio_output_list.append(
+    audio_output.AudioOutputManager(Conf.get_conf_data(ConfigKey.OUT_DEVICE_NAME),
+        speech_language=Conf.get_conf_data(ConfigKey.OUT_LANGUAGE))
+)
+
+Pipeline.register_elaboration_callback(reply_manager.callback_for_pipeline)
+Pipeline.register_output_callbacks("network", network.callback_for_pipeline)
+Pipeline.register_output_callbacks("audio_output", audio_output.callback_for_pipeline)
+
+print_service_status()
+any_mic_active = any(mic.is_device_active() for mic in audio_input.mic_list)
+
 #--------------------- Main ----------------------
-def main():
-    """
-    Starting function where the basic components will be initialized,
-    based on the choices made in the configuration file.
-    Mqtt, microphone, audio output.
-    """
-    network.NetworkManager.mqtt_connect_to_broker(
-        username=Conf.get_conf_data(ConfigKey.MQTT_USERNAME),
-        password=Conf.get_conf_data(ConfigKey.MQTT_PASSWORD),
-        host=Conf.get_conf_data(ConfigKey.MQTT_HOST),
-        port=Conf.get_conf_data(ConfigKey.MQTT_PORT)
-    )
-
-    #MIC_NAME and RECOGNITION_LANGUAGE will be a list to init more then one mic at time
-    audio_input.mic_list.append(
-        audio_input.MicManager(mic_name=Conf.get_conf_data(ConfigKey.MIC_NAME),
-            recognition_language=Conf.get_conf_data(ConfigKey.RECOGNITION_LANGUAGE))
-    )
-
-    #OUT_DEVICE_NAME will be a list to init more then one mic at time
-    audio_output.audio_output_list.append(
-        audio_output.AudioOutputManager(Conf.get_conf_data(ConfigKey.OUT_DEVICE_NAME),
-            speech_language=Conf.get_conf_data(ConfigKey.OUT_LANGUAGE))
-    )
-
-    Pipeline.register_elaboration_callback(reply_manager.callback_for_pipeline)
-    Pipeline.register_output_callbacks("network", network.callback_for_pipeline)
-    Pipeline.register_output_callbacks("audio_output", audio_output.callback_for_pipeline)
-
-    print_service_status()
-    any_mic_active = any(mic.is_device_active() for mic in audio_input.mic_list)
-    while network.NetworkManager.mqtt_active or any_mic_active:
-        time.sleep(0.1)
+def main(message: str):
+    Pipeline.run(message)
 
 if __name__ == "__main__":
     main()
